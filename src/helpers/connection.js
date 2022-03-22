d14b7f189314c908edf6ab65b7d6ab0c2e97e208
Ryan McNair <ryan.mcnair@gmail.com>
2022-03-22 09:56:54 -0500

    firebase setup

---
+++ src/helpers/connection.js
@@ -0,0 +1,11 @@
+
+import firebase from 'firebase/app';
+import firebaseConfig from '../apiKeys';
+
+const firebaseApp = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+};
+
+export default firebaseApp;
